e3fe9265f23ebded4083f7592bf388b9074bc5fc
cosgais <50753255+cosgais@users.noreply.github.com>
2023-04-19 14:23:12 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update && apt install docker-compose unzip -y
+
+xhost +
+
+wget https://raw.githubusercontent.com/cosgais/MOAB/main/Dockerfile
+
+wget https://raw.githubusercontent.com/cosgais/MOAB/main/docker-compose.yml
+wget https://raw.githubusercontent.com/cosgais/MOAB/main/requirements.txt
+
+wget https://www.dataforth.com/media/softwaredownload/maq20/ApplicationSoftware/MAQ20_API_Python-master.zip
+
+unzip MAQ20_API_Python-master.zip -d .
+
+rm MAQ20_API_Python-master.zip
+docker-compose build
+docker-compose up
